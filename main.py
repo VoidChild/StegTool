1fc86e5dfb3f7dbe37de95c1cb3f1e88882a9c83
VoidChild <10260142+VoidChild@users.noreply.github.com>
2023-07-03 19:26:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+
+import tkinter as tk
+from tkinter import filedialog
+
+def upload_image():
+    file_path = filedialog.askopenfilename()
+    print(f"Image uploaded from: {file_path}")
+
+root = tk.Tk()
+root.title("Image Uploader")
+
+upload_button = tk.Button(root, text="Upload Image", command=upload_image)
+upload_button.pack()
+
+root.mainloop()
